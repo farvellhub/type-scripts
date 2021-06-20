7f55b61f97c55b5849e18b6d36e0fd2730609295
farvell <farvellsmurf@gmail.com>
2021-06-20 21:35:42 +0200

    added spinWords kata

---
+++ src/spinwords.ts
@@ -0,0 +1,15 @@
+// const spinWords = (str) => {
+//   return str.split(" ").map((w) => {
+//     return (w.length > 4) ? w.split("").reverse().join("") : w;
+//   }).join(" ");
+// };
+
+const spinWords = ( phrase: string ) => {
+  return phrase.split( " " ).map(( word: string ) => {
+      return ( word.length > 4 )
+          ? word.split( "" ).reverse().join( "" )
+          : word;
+  }).join( " " );
+};
+
+export default spinWords;
